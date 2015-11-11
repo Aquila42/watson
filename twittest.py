97bb6a447da3944193edd99cc57c7f8834cd6e25
oksmartypants <cep2141@columbia.edu>
2015-11-11 16:31:34 -0500

    New version that pulls ~1k

--- twittest.py
+++ twittest.py
@@ -11,7 +11,6 @@ auth.set_access_token("17377774-b8FFONpU6OoZ2FOcQI5GbNNaFlDJ25hubOFxU9sVi", "USm
 
 api = tweepy.API(auth)
 
-
 overall=0
 while overall<10000:
     try:
@@ -30,27 +29,31 @@ while overall<10000:
             sn=user.screen_name
             ln=user.lang
             img=user.profile_image_url
-        ct=0
-        #for each tweet in the max amount (200) for the user, not including RTs
-        #this will give you (Prev tweet amount)-(Tweets that were RTs), so you'll probably get back <200.
-        timeline=api.user_timeline(screen_name=sn,count=200,include_rts=False)
-        for tweet in timeline:
-            ct+=1
-        #if user set lang to english and over 100 tweets from last part
-        #make file for each user found; includes screen name, pic, tweets
-        if ln=="en" and ct>=100:
-            print "name: ", sn
-            print "lang: ",ln
-            f=open(sn+'.txt', 'a')
-            f.write(sn+"\n")
-            img=img.replace("normal", "400x400")
-            f.write(img+"\n")
-            timeline=api.user_timeline(screen_name=sn,count=200, include_rts=False) #maximumcount of tweet
-            for tweet in timeline:
-                tw = tweet.text
-                f.write(tw.encode("utf-8")+"\n")
-            f.close()
-            snholder.append(sn)
-            overall+=1
+        if ln=="en":
+            #for each tweet in the max amount (200) for the user, not including RTs
+            #this will give you (Prev tweet amount)-(Tweets that were RTs), so you'll probably get back <200.
+            ct=0
+            for pg in xrange(1,6):
+                timeline=api.user_timeline(screen_name=sn,count=200, page=pg)
+                for tweet in timeline:
+                    ct+=1
+            print ct
+            #if user set lang to english and over 100 tweets from last part
+            #make file for each user found; includes screen name, pic, tweets
+            if ct>=900:
+                print "name: ", sn
+                f=open(sn+'.txt', 'a')
+                f.write(sn+"\n")
+                img=img.replace("normal", "400x400")
+                f.write(img+"\n")
+                writect=0
+                for pg in xrange(1,6):
+                    timeline=api.user_timeline(screen_name=sn,count=200, page=pg) #maximumcount of tweet
+                    for tweet in timeline:
+                        tw = tweet.text
+                        f.write(tw.encode("utf-8")+"\n")
+                        writect+=1
+                f.close()
+                overall+=1
     except:
-        print 'Not a valid account'
+        print "No account found"
